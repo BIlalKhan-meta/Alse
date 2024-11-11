dc2e9dd70f88642795be8bb1c76b5095d0a87bd9
Muhammad Ali <muhammad.ali@metafroliclabs.com>
2024-11-11 16:33:17 +0500

    About Us Added

--- src/screens/AboutUs/index.tsx
+++ src/screens/AboutUs/index.tsx
@@ -1,76 +1,43 @@
-import { useState } from 'react';
-import {
-  ScrollView,
-  View,
-} from 'react-native';
-
-import * as yup from 'yup';
+import {ScrollView, View} from 'react-native';
 import styles from './styles';
 import HeaderComponent from '../../components/HeaderComponent';
 import Card from '../../components/Card';
 import InterRegular from '../../components/Text/InterRegular';
+import {useEffect, useState} from 'react';
+import {getAboutUs} from '../../api/menu';
+import Loader from '../../components/Loader';
 
+const AboutUs: React.FC = () => {
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(false);
 
-const AboutUs: React.FC = ({ navigation }) => {
-
-  const [submitted, setSubmitted] = useState<boolean>(false)
-  const [messageSubmittedModal, setMessageSubmittedModal] = useState<boolean>(false)
-
-  interface FormValues {
-    name: string,
-    contactNo: string,
-    email: string,
-    subject: string,
-    message: string;
-  }
-
-  const initialValues = {
-    name: '',
-    contactNo: '',
-    email: '',
-    subject: '',
-    message: ''
+  const getData = async () => {
+    setLoading(true);
+    await getAboutUs()
+      .then(res => {
+        if (res?.data) {
+          setData(res?.data?.data?.content);
+        }
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   };
 
-  const validationSchema: yup.AnySchema<FormValues> = yup.object().shape({
-    name: yup.string().required('First Name is required'),
-    contactNo: yup.string().required('Contact Number is required'),
-    email: yup
-      .string()
-      .email('Email is wrong !! TRY AGAIN')
-      .required('Email is required'),
-    subject: yup.string().required('Subject is required'),
-    message: yup.string().required('Message is required'),
-  });
+  useEffect(() => {
+    getData();
+  }, []);
 
-  const handleSubmit = (values: object, { resetForm }: { resetForm: () => void }) => {
-    console.log("CONTACT ADMIN FORM SUBMITTED")
-    setMessageSubmittedModal(true)
-    resetForm()
-    // navigation.navigate("Login")
-
+  if (loading) {
+    return <Loader />;
   }
 
   return (
-    <>
-      <View style={{ paddingHorizontal: 15, paddingTop: 30 }}>
-
-        <HeaderComponent
-          label={'About Us'}
-
-        />
-      </View>
-      <ScrollView
-        showsVerticalScrollIndicator={false}
-      >
-        <Card style={styles.container}>
-          <InterRegular style={styles.adddetailsheading}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget odio. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate viverra justo commodo. Proin sodales pulvinar tempor.viverra justo commodo.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis.
-          </InterRegular>
-        </Card>
-      </ScrollView>
-
-    </>
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <Card style={styles.container}>
+        <InterRegular style={styles.adddetailsheading}>{data}</InterRegular>
+      </Card>
+    </ScrollView>
   );
 };
 
--- src/screens/AboutUs/styles.tsx
+++ src/screens/AboutUs/styles.tsx
@@ -1,24 +1,20 @@
-import { StyleSheet } from 'react-native';
-import { colors } from '../../utils/theme';
-import { fontSizes, vh, vw } from '../../constant';
+import {StyleSheet} from 'react-native';
+import {colors} from '../../utils/theme';
+import {fontSizes, vh, vw} from '../../constant';
 
 const styles = StyleSheet.create({
   scrollview: {
-    backgroundColor: colors.white
+    backgroundColor: colors.white,
   },
   container: {
-    paddingHorizontal: vw * 6,
-    paddingBottom: vh * 4,
-    margin: vh * 2
-    // backgroundColor:colors.red,
+    paddingHorizontal: vw * 4,
+    paddingVertical: vh * 2,
+    margin: vh * 2,
   },
   adddetailsheading: {
-    // marginTop: vh * 1.5,
     color: colors.darkGray,
-    fontSize: fontSizes.f14
-
-  }
-
+    fontSize: fontSizes.f14,
+  },
 });
 
 export default styles;
