0cb6638d301e8ea74e32dbec9835b6b880a0c053
Shahid-awan <shahidawan@gmail.com>
2026-01-05 06:56:51 +0500

    changes

--- metro.config.js
+++ metro.config.js
@@ -1,14 +1,14 @@
 const {
-    wrapWithReanimatedMetroConfig,
-  } = require('react-native-reanimated/metro-config');
+  wrapWithReanimatedMetroConfig,
+} = require('react-native-reanimated/metro-config');
 
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
 /**
  * Metro configuration
  * https://reactnative.dev/docs/metro
  *
- * @type {import('metro-config').MetroConfig}
+ * @type {import('@react-native/metro-config').MetroConfig}
  */
 const config = {};
 
