a06c262a59607a491f20676d8b72ed69db79ad9d
Naaman Saif <xaiflaghari2013@gmail.com>
2026-01-14 16:55:02 +0500

    Fixed ios builds

---
+++ ios/Alenga/Alenga-Bridging-Header.h
@@ -0,0 +1,12 @@
+#ifndef Alenga_Bridging_Header_h
+#define Alenga_Bridging_Header_h
+
+#import <React/RCTBridgeModule.h>
+#import <React/RCTBundleURLProvider.h>
+#import <React/RCTRootView.h>
+
+// Use the standard umbrella import for 0.79+
+#import <React_RCTAppDelegate/RCTAppDelegate.h>
+
+
+#endif
